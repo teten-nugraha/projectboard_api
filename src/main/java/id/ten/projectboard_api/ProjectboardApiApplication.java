b8cc4b48a641971284905db9d0476034b9a8d7be
Teten Nugraha <teten.nugraha18@gmail.com>
2021-12-16 10:47:05 +0700

    first launch

---
+++ src/main/java/id/ten/projectboard_api/ProjectboardApiApplication.java
@@ -0,0 +1,13 @@
+package id.ten.projectboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ten/projectboard_api/ProjectboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.projectboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
